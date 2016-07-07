650f0e4785e6f033cb9f381c86a27a8756dea0f5
runwithranvit <Runwith@Ranvits-MacBook-Pro.local>
2016-07-07 10:00:23 -0700

    First commit

---
+++ myFirstPython.py
@@ -0,0 +1,17 @@
+# This program says hello and asks for my name
+
+print('Hello World!')
+print('What is your name?')
+
+myName = input()
+
+print('It is nice to meet you, ' + myName)
+
+print('The length of your name is: ' + str(len(myName)))
+
+print('How old are you?')
+
+myAge = input()
+
+print('You will be ' + str(int(myAge) + 1) + ' next year!')
+
